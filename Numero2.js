0fab498497099ea2030b16d7b3e847c18e342540
Tuars1 <166967951+Tuars1@users.noreply.github.com>
2024-06-29 09:54:18 -0500

    Add files via upload

---
+++ Numero2.js
@@ -0,0 +1,22 @@
+﻿const question = document.querySelector(".question");
+const gif = document.querySelector(".gif")
+const yesBtn = document.querySelector(".yes-btn")
+const noBtn = document.querySelector(".no-btn")
+
+
+yesBtn.addEventListener("click", ()=> {
+    question.innerHTML = "Looo sabia ";
+    gif.src="https://media.tenor.com/kBDMplHmLb8AAAAC/cat-cat-meme.gif" /*el gif*/
+})
+
+noBtn.addEventListener("mouseover", ()=> {
+    const noBtnRect = noBtn.getBoundingClientRect();
+    const maxX = window.innerWidth - noBtnRect.width;
+    const maxY = window.innerHeight - noBtnRect.height;
+
+    const randomX = Math.floor(Math.random() * maxX)
+    const randomY = Math.floor(Math.random() * maxY)
+
+    noBtn.style.left = randomX + "px"; /*lo mando el No a un lado ramdon*/ 
+    noBtn.style.top = randomY + "px"
+})
